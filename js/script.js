9636daba59f3f70ef1920dd3e3f5626941b9ca90
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-03-10 18:52:12 +0100

    add hamburger and rwd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const activatePage = () => {
-    const links = document.querySelectorAll('.menu__link')
+    const menuLinks = document.querySelectorAll('.menu__link')
+    const footerLinks = document.querySelectorAll('.footer__link--menu')
 
-    for (let link of links) {
+    for (let link of menuLinks) {
 
         link.addEventListener('click', () => {
             const activeLink = document.querySelector('.activeLink')
@@ -16,6 +17,32 @@ const activatePage = () => {
             activateSection.classList.add('activeSection')
         })
     }
+
+    for (let link of footerLinks) {
+
+        link.addEventListener('click', () => {
+            const activeSection = document.querySelector('.activeSection')
+            const clickedLink = link.getAttribute('href').replace('#', '')
+            const activateSection = document.querySelector('.' + clickedLink)
+
+            activeSection.classList.remove('activeSection')
+
+            activateSection.classList.add('activeSection')
+        })
+    }
 }
 
 activatePage()
+
+const hamburger = () => {
+    const btnHamburger = document.querySelector('.hamburger')
+    const menu = document.querySelector('.menu__list')
+
+    btnHamburger.addEventListener('click', () => {
+        console.log('dupa');
+        btnHamburger.classList.toggle('is-active')
+        menu.classList.toggle('activeMenuList')
+    })
+}
+
+hamburger()
