f765a373708c8e26f0894a3b9f7c0c69ff4a0bcc
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-05-14 19:54:46 +0200

    change code and styles, add new project in Portfolio

--- js/script.js
+++ js/script.js
@@ -1,48 +1,43 @@
-const activatePage = () => {
-    const menuLinks = document.querySelectorAll('.menu__link')
-    const footerLinks = document.querySelectorAll('.footer__link--menu')
+const eventListener = () => {
+    const menuLinks = document.querySelectorAll(".menu__link");
+    const footerLinks = document.querySelectorAll(".footer__link--menu");
+    const hamburger = document.querySelector(".hamburger");
+    const menuList = document.querySelector(".menu__list");
 
     for (let link of menuLinks) {
+        link.addEventListener("click", () => {
+            const activeLink = document.querySelector(".activeLink");
+            const activeSection = document.querySelector(".activeSection");
+            const clickedLink = link.getAttribute("href").replace("#", "");
+            const activateSection = document.querySelector("." + clickedLink);
 
-        link.addEventListener('click', () => {
-            const activeLink = document.querySelector('.activeLink')
-            const activeSection = document.querySelector('.activeSection')
-            const clickedLink = link.getAttribute('href').replace('#', '')
-            const activateSection = document.querySelector('.' + clickedLink)
+            activeLink.classList.remove("activeLink");
+            activeSection.classList.remove("activeSection");
+            link.classList.add("activeLink");
+            activateSection.classList.add("activeSection");
 
-            activeLink.classList.remove('activeLink')
-            activeSection.classList.remove('activeSection')
-
-            link.classList.add('activeLink')
-            activateSection.classList.add('activeSection')
-        })
+            if (window.innerWidth <= 992) {
+                menuList.classList.remove("activeMenuList");
+                hamburger.classList.remove("is-active");
+            }
+        });
     }
 
     for (let link of footerLinks) {
+        link.addEventListener("click", () => {
+            const activeSection = document.querySelector(".activeSection");
+            const clickedLink = link.getAttribute("href").replace("#", "");
+            const activateSection = document.querySelector("." + clickedLink);
 
-        link.addEventListener('click', () => {
-            const activeSection = document.querySelector('.activeSection')
-            const clickedLink = link.getAttribute('href').replace('#', '')
-            const activateSection = document.querySelector('.' + clickedLink)
-
-            activeSection.classList.remove('activeSection')
-
-            activateSection.classList.add('activeSection')
-        })
+            activeSection.classList.remove("activeSection");
+            activateSection.classList.add("activeSection");
+        });
     }
-}
 
-activatePage()
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("is-active");
+        menuList.classList.toggle("activeMenuList");
+    });
+};
 
-const hamburger = () => {
-    const btnHamburger = document.querySelector('.hamburger')
-    const menu = document.querySelector('.menu__list')
-
-    btnHamburger.addEventListener('click', () => {
-        console.log('dupa');
-        btnHamburger.classList.toggle('is-active')
-        menu.classList.toggle('activeMenuList')
-    })
-}
-
-hamburger()
+eventListener();
